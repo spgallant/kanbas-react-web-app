365b1d2a2dd30f538661fff19e7b6b0c28b8f7a5
spgallant <sarahpgallant@gmail.com>
2024-05-09 09:52:37 -0400

    first commit

---
+++ src/Labs/index.tsx
@@ -0,0 +1,11 @@
+import Lab1 from "./Lab1";
+
+export default function Labs() {
+    return (
+        //Give a label to component, add header, ref lab1 import
+        <div id="wd-labs">  
+            <h1>Labs</h1>
+            <Lab1 />
+        </div>
+    );
+}
