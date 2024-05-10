95721281259d707bb57f4ab91d770624b0d6a92f
spgallant <sarahpgallant@gmail.com>
2024-05-10 12:37:10 -0400

    Lab exercise completion

---
+++ src/Labs/Lab2/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Lab2() {
+    return (
+      <div>
+        <h2>Lab 2</h2>
+      </div>
+    );
+  }
+  
+
+  
---
+++ src/Labs/Lab3/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Lab3() {
+    return (
+      <div>
+        <h2>Lab 3</h2>
+      </div>
+    );
+  }
