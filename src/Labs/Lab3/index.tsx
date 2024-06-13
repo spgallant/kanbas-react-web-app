1ca63b00a9325bcc08ec43f328934cc6a5c7d1fc
spgallant <sarahpgallant@gmail.com>
2024-06-12 22:30:47 -0400

    A3 Lab thru 2.6.2

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -17,7 +17,19 @@ import ForLoops from './ForLoops';
 import MapFunction from './MapFunction';
 import FindFunction from './FindFunction';
 import FindIndex from './FindIndex';
-
+import FilterFunction from './FilterFunction';
+import JsonStringify from './JsonStringify';
+import House from './House';
+import TodoList from './todos/TodoList';
+import Spreading from './Spreading';
+import Destructing from './Destructing';
+import FunctionDestructing from './FunctionDestructing';
+import DestructingImports from './DestructingImports';
+import Classes from './Classes';
+import Styles from './Styles';
+import Add from './Add';
+import Square from './Square';
+import Highlight from './Highlight';
 
 export default function Lab3() {
     return (
@@ -41,6 +53,29 @@ export default function Lab3() {
         <MapFunction/>
         <FindFunction/>
         <FindIndex/>
+        <FilterFunction/>
+        <JsonStringify/>
+        <House/>
+        <TodoList/>
+        <Spreading/>
+        <Destructing/>
+        <FunctionDestructing/>
+        <DestructingImports/>
+        <Classes/>
+        <Styles/>
+        <Add a={3} b={4} />
+
+        <h4>Square of 4</h4>
+        <Square>4</Square>
+        <hr />
+
+        <Highlight>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+          vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+        </Highlight>
+
+
+
       </div>
     );
   }
