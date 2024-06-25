e4b5f539a47613792977c919561f3b388e4f54d1
spgallant <sarahpgallant@gmail.com>
2024-06-25 16:42:22 -0400

    partial Kanbas 3.1.1

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles.css';
 import { Link } from "react-router-dom";
 import * as db from "../Database";
@@ -7,13 +7,56 @@ import * as db from "../Database";
 
 
 export default function Dashboard() {
-    const courses = db.courses;
+    const [courses, setCourses] = useState(db.courses);
+
+    const [course, setCourse] = useState<any>({
+      _id: "0", name: "New Course", number: "New Number",
+      startDate: "2023-09-10", endDate: "2023-12-15",
+      imageName: "reactjs.jpg", description: "New Description"
+    });
+
+    const addNewCourse = () => {
+      const newCourse = { ...course,
+                          _id: new Date().getTime().toString() };
+      setCourses([...courses, { ...course, ...newCourse }]);
+    };
+  
+
+
+
 
     console.log(courses);
 
     return (
       <div id="wd-dashboard">
         <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+
+        <h5>New Course
+          <button 
+            className="btn btn-primary float-end"
+            id="wd-add-new-course-click"
+            onClick={addNewCourse} > 
+            Add 
+          </button> 
+        </h5>
+
+        <br />
+        <div className="card">
+          <div className="card-body ">
+            <input value={course.name} className="form-control mb-2" 
+              onChange={(e) => setCourse({ ...course, name: e.target.value }) }/>
+            <input value={course.number} className="form-control mb-2" />
+            <input value={course.imageName} className="form-control mb-2" />
+            <input type="datetime-local" id="wd-available-from" className="form-control mb-2" value={course.startDate}/>
+            <input type="datetime-local" id="wd-due-date" className="form-control mb-2" value={course.dueDate}/>
+            <textarea value={course.description} className="form-control"/>
+          </div>
+        </div>
+        <hr />
+
+
+
+
         <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> 
         <hr />
         <div id="wd-dashboard-courses"  className="row">
