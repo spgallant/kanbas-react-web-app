9b3f79ee7c7936d460a9d2e52f1fd291ac1c83d0
spgallant <sarahpgallant@gmail.com>
2024-06-28 09:40:04 -0400

    kanbas thru 3.1

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -21,7 +21,23 @@ export default function Dashboard() {
       setCourses([...courses, { ...course, ...newCourse }]);
     };
   
+    const deleteCourse = (courseId: string) => {
+      setCourses(courses.filter((course) => course._id !== courseId));
+    };
 
+    const updateCourse = () => {
+      setCourses(
+        courses.map((c) => {
+          if (c._id === course._id) {
+            return course;
+          } else {
+            return c;
+          }
+        })
+      );
+    };
+  
+  
 
 
 
@@ -38,18 +54,57 @@ export default function Dashboard() {
             onClick={addNewCourse} > 
             Add 
           </button> 
+
+          <button 
+            className="btn btn-warning float-end me-2"
+            onClick={updateCourse} id="wd-update-course-click">
+            Update
+          </button>
+
         </h5>
 
         <br />
-        <div className="card">
-          <div className="card-body ">
-            <input value={course.name} className="form-control mb-2" 
-              onChange={(e) => setCourse({ ...course, name: e.target.value }) }/>
-            <input value={course.number} className="form-control mb-2" />
-            <input value={course.imageName} className="form-control mb-2" />
-            <input type="datetime-local" id="wd-available-from" className="form-control mb-2" value={course.startDate}/>
-            <input type="datetime-local" id="wd-due-date" className="form-control mb-2" value={course.dueDate}/>
-            <textarea value={course.description} className="form-control"/>
+        
+        <div className="col">
+          <div className="card">
+            <div className="card-body ">
+
+              <div className="mb-3">
+                <label htmlFor="wd-name-dash" className="fw-bold mb-1">Course Name </label><br/>
+                <input value={course.name} className="form-control mb-2" id="wd-name-dash"
+                  onChange={(e) => setCourse({ ...course, name: e.target.value }) }/>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="wd-description-dash" className="fw-bold mb-1">Description</label><br/>
+                <textarea value={course.description} className="form-control" id="wd-description-dash"
+                  onChange={(e) => setCourse({ ...course, description: e.target.value }) }/>
+              </div>
+              <div className='row'>
+                <div className="mb-3 col">
+                  <label htmlFor="wd-id-dash" className="fw-bold mb-1">Course Number </label><br/>
+                  <input value={course.number} className="form-control mb-2" id="wd-id-dash"
+                    onChange={(e) => setCourse({ ...course, number: e.target.value }) }/>
+                </div>
+                <div className="mb-3 col">
+                  <label htmlFor="wd-image-dash" className="fw-bold mb-1">Course Image </label><br/>
+                  <input value={course.imageName} className="form-control mb-2" id="wd-image-dash"
+                    onChange={(e) => setCourse({ ...course, imageName: e.target.value }) }/>
+                </div>
+              </div>
+              <div className='row'>
+                <div className="mb-3 col">
+                  <label htmlFor="wd-available-from-dash" className="fw-bold mb-1">Available From </label><br/>
+                  <input type="datetime-local" id="wd-available-from-dash" className="form-control mb-2" value={course.startDate}
+                    onChange={(e) => setCourse({ ...course, startDate: e.target.value }) }/>
+                </div>
+                <div className="mb-3 col">
+                  <label htmlFor="wd-due-date-dash" className="fw-bold mb-1">Due Date </label><br/>
+                  <input type="datetime-local" id="wd-due-date-dash" className="form-control mb-2" value={course.dueDate}
+                    onChange={(e) => setCourse({ ...course, endDate: e.target.value }) }/>
+                </div>
+              </div>
+          
+            </div>
           </div>
         </div>
         <hr />
@@ -79,7 +134,33 @@ export default function Dashboard() {
                       <p className="wd-dashboard-course-title card-text" style={{maxHeight: 53, overflow: "hidden"}}>
                         {course.description}
                       </p>
+
                       <Link to={`/Kanbas/Courses/${course._id}/Home`}  className="btn btn-primary"> Go </Link>
+
+
+                      <button onClick={(event) => {
+                        event.preventDefault();
+                        deleteCourse(course._id);
+                        }} 
+                        className="btn btn-danger float-end"
+                        id="wd-delete-course-click">
+                        Delete
+                      </button>
+
+                      <button id="wd-edit-course-click"
+                        onClick={(event) => {
+                          event.preventDefault();
+                          setCourse(course);
+                        }}
+                        className="btn btn-warning me-2 float-end" >
+                        Edit
+                      </button>
+
+
+
+
+
+
                     </div>
                   </div>
                 </Link>
@@ -89,6 +170,7 @@ export default function Dashboard() {
           </div>
         </div>
         <br/><br/>
+        
       </div>
     );
 
